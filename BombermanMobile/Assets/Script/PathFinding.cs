748be0dcd22f847f2227c9e43d87becbdd4725c0
DoriannX <theodor.xav@gmail.com>
2024-02-27 17:09:42 +0100

    Unit Manager

--- BombermanMobile/Assets/Script/PathFinding.cs
+++ BombermanMobile/Assets/Script/PathFinding.cs
@@ -1,16 +1,54 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
 public class PathFinding : MonoBehaviour
 {
-    
+    private enum Team {Player, Ennemy};
+    private enum Type { Suicidal, Mortar, Classic, Bowman, Minelayer, Funky};
+    [SerializeField] Team _characterTeam = Team.Player;
+    [SerializeField] Type _characterType = Type.Classic; 
+    List<GameObject> _enemiesUnit = new List<GameObject>();
     private Vector3 _touchPos = Vector3.zero;
     private NavMeshAgent _agent = null;
 
     private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _enemiesUnit = UnitManager.Instance.EnemiesUnits;
+    }
+
+    private GameObject GetTarget()
+    {
+        GameObject target = null;
+        switch (_characterType)
+        {
+            case Type.Suicidal:
+                target = UnitManager.Instance.GetClosest(gameObject);
+                break;
+            case Type.Mortar:
+                target = UnitManager.Instance.GetFarthest(gameObject);
+                break;
+            case Type.Classic:
+                target = UnitManager.Instance.GetClosest(gameObject);
+                break;
+            case Type.Bowman:
+                target = UnitManager.Instance.GetClosest(gameObject);
+                break;
+            case Type.Minelayer:
+                //target = 
+                break;
+            case Type.Funky:
+                break;
+        }
+        return target;
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        //Vector3 randomPos = new Vector3(Random.Range(-10, 10), Random.Range();
+        return Vector3.zero;
     }
 
     public void MoveTo(InputAction.CallbackContext ctx)
---
+++ BombermanMobile/Assets/Script/UnitManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitManager : MonoBehaviour
+{
+    public static UnitManager Instance;
+    public List<GameObject> EnemiesUnits { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance == null)
+            Instance = this;
+    }
+    
+    public GameObject GetClosest(GameObject unit)
+    {
+        
+        GameObject closest = EnemiesUnits[0];
+
+        foreach(GameObject enemyUnit in EnemiesUnits)
+        {
+            if(Vector3.Distance(unit.transform.position, enemyUnit.transform.position) < Vector3.Distance(unit.transform.position, closest.transform.position))
+            {
+                closest = enemyUnit;
+            }
+        }
+        print(closest);
+        return closest;
+    }
+
+    public GameObject GetFarthest(GameObject unit)
+    {
+        GameObject farthest = EnemiesUnits[0];
+        foreach (GameObject enemyUnit in EnemiesUnits)
+        {
+            if (Vector3.Distance(unit.transform.position, enemyUnit.transform.position) > Vector3.Distance(unit.transform.position, farthest.transform.position))
+            {
+                farthest = enemyUnit;
+            }
+        }
+        print(farthest);
+        return farthest;
+    }
+
+    public GameObject GetRandom(GameObject unit)
+    {
+        GameObject random = EnemiesUnits[Random.Range(0, EnemiesUnits.Count)];
+        
+        print(random);
+        return random;
+    }
+}
