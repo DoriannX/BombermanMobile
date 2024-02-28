90fbda01d800c2763cad496eb2d47df93ac636db
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-02-28 14:22:57 +0100

    place offset implemented

--- BombermanMobile/Assets/GameUI/GoatPortrait.cs
+++ BombermanMobile/Assets/GameUI/GoatPortrait.cs
@@ -10,15 +10,22 @@ public class GoatPortrait : MonoBehaviour, IPointerDownHandler
     private Transform _unitVisual; //unit that is following finger
 
     [SerializeField] private GameObject _unitPrefab;
+
+    private Vector2 _placeOffset = Vector2.zero;
     
     private void Awake()
     {
         
     }
 
+    private void Start()
+    {
+        //_placeOffset = new Vector2(0, Screen.height / 15f);
+    }
+
     public void FollowFinger()
     {
-        Vector2 fingerPos = InputManager.Instance.LastTouchPosition;
+        Vector2 fingerPos = InputManager.Instance.LastTouchPosition + _placeOffset;
         if (_isClicked)
         {
             if (!_alreadyFollowing)
@@ -27,13 +34,14 @@ public class GoatPortrait : MonoBehaviour, IPointerDownHandler
                 GameObject newObject = new GameObject();
                 newObject.AddComponent<Image>();
                 newObject.GetComponent<Image>().sprite = GetComponent<Image>().sprite;
-                _unitVisual = Instantiate(newObject, InputManager.Instance.LastTouchPosition, Quaternion.identity, transform.parent.parent).transform;
+                _unitVisual = Instantiate(newObject, fingerPos, Quaternion.identity, transform.parent.parent).transform;
                 Destroy(newObject);
             }
             else
             {
                 _unitVisual.position = new Vector3(fingerPos.x, fingerPos.y, 0);
             }
+            _unitVisual.GetComponent<Image>().color = (CanSpawnUnitAt(fingerPos)) ? Color.green : Color.red;
         }
     }
 
@@ -41,7 +49,7 @@ public class GoatPortrait : MonoBehaviour, IPointerDownHandler
     {
         if (_unitVisual)
         {
-            SpawnUnit(InputManager.Instance.LastTouchPosition);
+            SpawnUnit(InputManager.Instance.LastTouchPosition + _placeOffset);
             Destroy(_unitVisual.gameObject);
             _unitVisual = null;
         }
@@ -55,11 +63,33 @@ public class GoatPortrait : MonoBehaviour, IPointerDownHandler
         Ray ray = Camera.main.ScreenPointToRay(new Vector3(spawnPosition.x, spawnPosition.y, 0));
         if (Physics.Raycast(ray, out RaycastHit hitData))
         {
-            if (hitData.collider.CompareTag("Ground"))
+            if (CanSpawnUnitAt(spawnPosition))
             {
                 Instantiate(_unitPrefab, hitData.point, Quaternion.identity);
             }
         }
+    }
+
+    private bool CanSpawnUnitAt(Vector2 spawnPosition)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(spawnPosition.x, spawnPosition.y, 0));
+        if (Physics.Raycast(ray, out RaycastHit hitData))
+        {
+            if (hitData.collider.CompareTag("Ground"))
+            {
+                float sphereRadius = 0.8f;
+                foreach (Collider collider in Physics.OverlapSphere(hitData.point, sphereRadius))
+                {
+
+                    if (!collider.CompareTag("Ground"))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
     }
 
     public void OnPointerDown(PointerEventData eventData)
