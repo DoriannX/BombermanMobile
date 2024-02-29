27d4844bdaa80929904918cdd529b300fcfdbeee
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-02-29 10:56:04 +0100

    removed useless code

--- BombermanMobile/Assets/IA/AIBowman.cs
+++ BombermanMobile/Assets/IA/AIBowman.cs
@@ -4,15 +4,4 @@ using UnityEngine;
 
 public class AIBowman : AIUnit
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
