ea6e7dacb83d7df33615fcb750d4e28d05b3fded
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-03-01 16:03:22 +0100

    can throw bomb

--- BombermanMobile/Assets/IA/AIBowman.cs
+++ BombermanMobile/Assets/IA/AIBowman.cs
@@ -4,13 +4,63 @@ using UnityEngine;
 
 public class AIBowman : AIUnit
 {
+    PathFinding _pathFinding;
+
+    public override GameObject GetTarget()
+    {
+        GameObject target = UnitManager.Instance.GetClosest(gameObject, CurrentTeam);
+
+        if (target != null)
+        {
+            _currentTarget = target;
+            if (Vector3.Distance(transform.position, _currentTarget.transform.position) > _range)
+            {
+                _agent.destination = target.transform.position;
+            } 
+            else
+            {
+                _agent.destination = transform.position;
+            }
+            
+        }
+        else
+        {
+            _agent.destination = transform.position;
+        }
+        return target;
+    }
+
     public override void Start()
     {
         base.Start();
+        _pathFinding = GetComponent<PathFinding>();
     }
 
-    public override void Awake()
+    private void Update()
     {
-        base.Awake();
+        if (_currentTarget)
+        {
+            if (_pathFinding.IsCloseToEnnemy(_range, _currentTarget) || _pathFinding.HasWallInFront(2))
+            {
+                Attack();
+            }
+        }
+    }
+
+    public override void Attack()
+    {
+        if (CanAttack())
+        {
+            GameObject bomb = Instantiate(UnitManager.Instance.BombObject, gameObject.transform.position
+                + (_bombPlacementOffset.z * transform.forward)
+                + (_bombPlacementOffset.x * transform.right), Quaternion.identity);
+            bomb.GetComponent<Bomb>().TargetPosition = _currentTarget.transform.position;
+            bomb.GetComponent<Bomb>().TimeToExplode = _bombTimeToExplode;
+            bomb.GetComponent<Bomb>().ExplosionRange = _bombRange;
+            bomb.GetComponent<Bomb>().ExplosionDamage = _bombDamage;
+            bomb.GetComponent<Bomb>().CurrentTeam = CurrentTeam;
+            bomb.GetComponent<Bomb>().IsThrowed = true;
+            StartCoroutine(Reloading());
+        }
     }
 }
--- BombermanMobile/Assets/Script/Bomb.cs
+++ BombermanMobile/Assets/Script/Bomb.cs
@@ -16,10 +16,49 @@ public class Bomb : Unit
     private bool _exploding = true;
 
     [SerializeField] private Image _imageRadius;
-    
+
+    public bool IsThrowed = false;
+    public float BombSpeed = 3;
+    private Vector3 _basePosition;
+    private float _maxHeight = 4;
+    public Vector3 TargetPosition;
+    private float _baseHeight;
+    [SerializeField] private AnimationCurve _heightCurve;
+
+    private void Awake()
+    {
+        _baseHeight = transform.position.y;
+    }
+
     private void Start()
     {
+        if (IsThrowed)
+        {
+            _baseHeight = transform.position.y;
+            _basePosition = transform.position;
+            StartCoroutine(LaunchBomb());
+        }
+        else
+        {
+            Detonate();
+        }
+    }
+
+    private IEnumerator LaunchBomb()
+    {
+        while (Vector3.Distance(new Vector3(transform.position.x, TargetPosition.y, transform.position.z), TargetPosition) > .5f)
+        {
+            float totalDistance = Vector3.Distance(_basePosition, TargetPosition);
+            float currentDistance = Vector3.Distance(_basePosition, transform.position);
+            float proportion = currentDistance / totalDistance;
+            proportion = Mathf.Clamp01(proportion);
+
+            transform.position = new Vector3(transform.position.x, _baseHeight + (_heightCurve.Evaluate(proportion) * _maxHeight), transform.position.z);
+            transform.position += (TargetPosition - transform.position).normalized * BombSpeed * Time.deltaTime;
+            yield return null;
+        }
         Detonate();
+        yield return null;
     }
 
     private void Detonate()
@@ -29,7 +68,7 @@ public class Bomb : Unit
         _imageRadius.transform.localScale = new Vector3(1f, 1f, 1f) * ExplosionRange;
     }
 
-    IEnumerator OnDestroyBomb()
+    private IEnumerator OnDestroyBomb()
     {
         yield return new WaitForSeconds(TimeToExplode);
         ParticleManager.Instance.ExplodeParticle(transform.position);
