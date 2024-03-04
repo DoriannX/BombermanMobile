1b51da3b6dfeb70195ac549cca9dc7d95945affe
DoriannX <theodor.xav@gmail.com>
2024-03-04 11:40:55 +0100

    Update bonus.cs

--- BombermanMobile/Assets/Script/bonus.cs
+++ BombermanMobile/Assets/Script/bonus.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class bonus : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        Destroy(gameObject, 1);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DestroyBonus()
     {
-        
+
     }
 }
