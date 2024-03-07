83eb4b5189ac3ef8ae9adf27497e514675149c6f
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-03-07 01:11:53 +0100

    added last damage name to units

--- BombermanMobile/Assets/Script/Bomb.cs
+++ BombermanMobile/Assets/Script/Bomb.cs
@@ -83,6 +83,7 @@ public class Bomb : Unit
                 {
                     if (unit.CurrentTeam != CurrentTeam)
                     {
+                        unit.LastDamageSourceName = OwnerName;
                         unit.TakeDamage(ExplosionDamage);
                     }
                 }
