51b457c9949a9063df08943e55ecad63b4dc010e
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-03-07 15:49:13 +0100

    FIX

--- BombermanMobile/Assets/Script/KillFeedManager.cs
+++ BombermanMobile/Assets/Script/KillFeedManager.cs
@@ -6,6 +6,10 @@ public class KillFeedManager : MonoBehaviour
 {
     public static KillFeedManager Instance;
 
+    [SerializeField] private GameObject _hud;
+
+    [SerializeField] private List<string> _deathLines = new List<string>();
+
     private void Awake()
     {
         Instance = this;
@@ -14,5 +18,6 @@ public class KillFeedManager : MonoBehaviour
     public void NewKillFeed(string victim, string killer)
     {
         print(killer + " killed " + victim);
+        //Destroy(Instantiate(victim));
     }
 }
