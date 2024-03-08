9b57695cb47ddf817304a7cbab5b02b8dcc04fd6
DoriannX <theodor.xav@gmail.com>
2024-03-08 10:46:06 +0100

    started transition

--- BombermanMobile/Assets/Script/MainMenuScripts/MenuManager.cs
+++ BombermanMobile/Assets/Script/MainMenuScripts/MenuManager.cs
@@ -8,7 +8,8 @@ public class MenuManager : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        SceneTransition.Instance.StartTransition();
     }
 
     private void Start()
---
+++ BombermanMobile/Assets/Script/SceneTransition/SceneTransition.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneTransition : MonoBehaviour
+{
+
+    [SerializeField] Image _transitionImage;
+    float _i = 0;
+    bool _startedInTransition = false;
+    bool _startedOutTransition = false;
+    public static SceneTransition Instance;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+            _transitionImage.color = new Color(0, 0, 0, 255);
+        else
+            _transitionImage.color = new Color(0, 0, 0, 0);
+    }
+
+    public void StartTransition()
+    {
+        _i = 0;
+        _startedInTransition = true;
+    }
+    private void InTransition()
+    {
+        if (_startedInTransition)
+        {
+
+            _transitionImage.color = new Color(0, 0, 0, _i);
+            _i += Time.deltaTime;
+            if (_i >= 255)
+            {
+                _startedInTransition = false;
+                StartOutTransition();
+            }
+        }
+    }
+
+    private void StartOutTransition()
+    {
+        _i = 255;
+        _startedOutTransition = true;
+    }
+
+
+
+    private void OutTransition()
+    {
+        if (_startedOutTransition)
+        {
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            _transitionImage.color = new Color(0, 0, 0, _i);
+            _i -= Time.deltaTime;
+            if (_i >= 255)
+            {
+                _startedOutTransition = false;
+            }
+        }
+    }
+}
